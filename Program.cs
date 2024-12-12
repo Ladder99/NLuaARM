b2d086924be03264da74b0e372081ccc07635926
MRIIOT <cmisztur@mriiot.com>
2024-12-12 10:06:39 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+
+Console.WriteLine("NLUA ARM");
+
+NLua.Lua lua = new NLua.Lua();
+object result = null;
+result = lua.DoString("print(\"Hello from Lua!  Next object is empty and the one after that is 200.\");");
+Console.WriteLine(JsonConvert.SerializeObject(result));
+result = lua.DoString("return 100 + 100;");
+Console.WriteLine(JsonConvert.SerializeObject(result));
+
+Console.WriteLine("press any key to exit...");
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# NLuaARM
+```
+cd ~
+git clone https://github.com/ladder99/NLuaARM
+cd NLuaARM/NLuaARM
+
+docker build -f Dockerfile --tag=nluaarm:latest .
+
+docker run nluaarm:latest
+```
